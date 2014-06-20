5e9d77809b3b2296b13237cc3ca66f8f3a0b1d1c
root <root@raspberrypi.(none)>
2014-06-20 17:00:57 +0000

    some jafangiat:D

--- client.js
+++ client.js
@@ -12,28 +12,93 @@ socket.emit('checkRDB', {
 	ID: 1 // => 2 is human || 1 is raspbery pi
 });
 var led = 23 // pin number of led
-
-socket.on('changeStatus', function(message){
-	gpio.setup(led, gpio.DIR_OUT, write);	
+var inputStatus = false;
+socket.on('changeStatus', function(message)
+{
+	var power = message.power;
+	if(power)
+	{
+		gpio.setup(led, gpio.DIR_OUT, on);
+        }
+        else
+        {
+		gpio.setup(led, gpio.DIR_OUT, off);
+	}
 });
 
-function write() {
-    gpio.write(led, 0, function(err) {
+function on()
+ {
+    console.log(1);
+    gpio.write(led, true, function(err) {
         if (err) throw err;
-        console.log('LED=>power changed');
+        console.log('LED=>power on');
     });
 }
 
+function off()
+{
+    console.log(0);
+    gpio.write(led, false, function(err)
+    {
+        if (err) throw err;
+        console.log('LED=>power off');
+    });
+
+}
+//gpio.setup(7, gpio.DIR_IN, shalgham);
+
+function shalgham()
+{
+	console.log("hehehe");
+}
+
+//gpio.on('change', function(channel, value)
+//{
+//	console.log('fahmidam :D');
+//});
+//gpio.setup(7, gpio.DIR_IN, shalgham);
+
+//function readInput2(){
+//	while(true)
+//	{
+//		gpio.read(7,function(err, value)
+//		{
+//			if(value!=inputStatus)
+//			{
+//				socket.emit('statusMessage', {
+//				        power: value,
+//				        ID: 2
+//				});
+//			}
+//			inputStatus = value;
+//		});
+//	}
+//}
 //gpio.on('change', function(channel, value) {
 //	console.log('Channel ' + channel + ' value is now ' + value);
-//	gpio.setup(led, gpio.DIR_IN, readInput);
-//}
+//});
+
+function checkInput(){
+	gpio.read(7, function(err, value)
+	{
+		if(inputStatus != value)
+		{
+			socket.emit('statusMessage', {
+			        power: value,
+			        ID: 2
+			});
+
+		}
+        	inputStatus = value;
+        });
+}
+
 
 function readInput() {
     gpio.read(led, function(err, value) 
     {
     	console.log('The value is ' + value);
-	if(bool(value))
+	if(value)
 	{
 		socket.emit('statusMessage', {
 			power: 1,
@@ -48,7 +113,7 @@ function readInput() {
 		});
 	}
     });
-});
+}
 
 
 
