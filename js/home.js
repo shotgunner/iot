1dd6c2a282d60e5da8d97d9e00bb390120dfe292
amsoft <abolfazl.mahdizade@gmail.com>
2014-07-23 14:33:00 +0430

    human client node.js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,8 @@
 $(".button").on("click", function(){
     $(this).toggleClass("active");
+	var socket = io.connect('10.10.10.221:8080');
+	var powerStatus = $(this).hasClass("active");
+    socket.emit('statusMessage', {
+		power: powerStatus
+	});
 });
--- server.js
+++ server.js
@@ -1,3 +1,14 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+var server = connect().use(serveStatic(__dirname)).listen(8080);
+var io = require('socket.io').listen(server);
+
+
+io.sockets.on('connection', function (socket) {
+    console.log("vasl shod");
+    socket.on('statusMessage', function (message) {
+        console.log("I get message");
+        console.log(message);
+        
+    });
+});
