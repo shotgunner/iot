7dd2a6a44427e0d552814f36fa66d2ecb7b05d09
amsoft <abolfazl.mahdizade@gmail.com>
2014-07-23 15:06:34 +0430

    redis

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ var serveStatic = require('serve-static');
 var server = connect().use(serveStatic(__dirname)).listen(8080);
 var io = require('socket.io').listen(server);
 
+var redis = require('redis');
+
+function createClient(port, host, pass) {
+    var client = redis.createClient(port, host);
+    client.auth(pass);
+    return client;
+}
+
+var client = createClient('16379' , '127.13.101.2', 'ZTNiMGM0NDI5OGZjMWMxNDlhZmJmNGM4OTk2ZmI5');
+
 
 io.sockets.on('connection', function (socket) {
     console.log("vasl shod");
