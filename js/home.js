9fcf0a8d2dacce199d1c6b6d43b87e1218a2d1f6
shotgunner <mojtabaeuler@gmail.com>
2014-07-24 02:35:52 +0430

    adding client.py for raspbery step 1

--- js/home.js
+++ js/home.js
@@ -2,12 +2,13 @@ $(".button").on("click", function(){
     $(this).toggleClass("active");
 	var socket = io.connect('10.10.10.221:8080');
 	var powerStatus = $(this).hasClass("active");
+	socket.emit('checkRDB', {
+		ID: 2 // => 2 is human || 1 is raspbery pi
+	});
     socket.emit('statusMessage', {
 		power: powerStatus,
 		ID: 1
 	});
 	
-	socket.emit('checkRDB', {
-		ID: 2 // => 2 is human || 1 is raspbery pi
-	});
+	
 });
