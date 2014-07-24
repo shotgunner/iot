857862b7057bd1c369d276fd75ee715aa7203999
shotgunner <mojtabaeuler@gmail.com>
2014-07-24 15:44:53 +0430

    pi to human web browser reaction added

--- js/home.js
+++ js/home.js
@@ -1,14 +1,24 @@
+var socket = io.connect('10.10.10.221:8080');
+socket.emit('checkRDB', {
+	ID: 2 // => 2 is human || 1 is raspbery pi
+});
+
 $(".button").on("click", function(){
     $(this).toggleClass("active");
-	var socket = io.connect('10.10.10.221:8080');
+	
 	var powerStatus = $(this).hasClass("active");
-	socket.emit('checkRDB', {
-		ID: 2 // => 2 is human || 1 is raspbery pi
-	});
+	
     socket.emit('statusMessage', {
 		power: powerStatus,
 		ID: 1
-	});
-	
-	
+	});	
+});
+
+socket.on('changeStatus', function (message) {
+	console.log(message);
+	if(message.powerStatus == '1'){
+		$(".button").addClass("active");
+	}else{
+		$(".button").removeClass("active");
+	}
 });
